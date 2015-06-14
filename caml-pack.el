ac717510b65072380545d2c540d8550484fe96e7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-14 14:53:45 +0200

    Use use-package

--- caml-pack.el
+++ caml-pack.el
@@ -4,32 +4,30 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(tuareg
-                                       smartscan))
+(use-package 'smartscan)
 
-(require 'tuareg)
-(require 'smartscan)
+(use-package tuareg
+  :init
+  (add-hook 'tuareg-mode-hook (lambda () (smartscan-mode)))
 
-(add-hook 'tuareg-mode-hook (lambda () (smartscan-mode)))
+  (defun switch-to-caml-buffer ()
+    "Switch to the caml buffer if needed."
+    (interactive)
+    ;; will trigger a caml buffer if needed
+    (tuareg-run-process-if-needed)
+    (pop-to-buffer tuareg-interactive-buffer-name))
 
-(defun switch-to-caml-buffer ()
-  "Switch to the caml buffer if needed"
-  (interactive)
-  ;; will trigger a caml buffer if needed
-  (tuareg-run-process-if-needed)
-  (pop-to-buffer tuareg-interactive-buffer-name))
+  ;; display-buffer--same-window-action (to permit the swich keeping the same window)
+  ;; display-buffer--other-frame-action (in another frame)
 
-;; display-buffer--same-window-action (to permit the swich keeping the same window)
-;; display-buffer--other-frame-action (in another frame)
+  ;; multi line comments
+  (setq tuareg-comment-end-extra-indent 1)
 
-;; multi line comments
-(setq tuareg-comment-end-extra-indent 1)
+  ;; make the interactive buffer scroll if needed
+  (setq tuareg-interactive-scroll-to-bottom-on-output t)
 
-;; make the interactive buffer scroll if needed
-(setq tuareg-interactive-scroll-to-bottom-on-output t)
+  (define-key tuareg-mode-map (kbd "C-c C-z") 'switch-to-caml-buffer))
 
-(define-key tuareg-mode-map (kbd "C-c C-z") 'switch-to-caml-buffer)
 
 (provide 'caml-pack)
 ;;; caml-pack ends here
