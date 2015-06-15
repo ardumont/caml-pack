b89f97356d0151a3c9259d091d82fab01077b904
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-15 11:39:18 +0200

    Improve caml-pack startup.

--- caml-pack.el
+++ caml-pack.el
@@ -4,30 +4,22 @@
 
 ;;; Code:
 
-(use-package 'smartscan)
+(use-package smartscan)
 
 (use-package tuareg
-  :init
-  (add-hook 'tuareg-mode-hook (lambda () (smartscan-mode)))
+  :init (progn
+          (add-hook 'tuareg-mode-hook (lambda () (smartscan-mode)))
+          (defun switch-to-caml-buffer ()
+            "Switch to the caml buffer if needed."
+            (interactive)
+            ;; will trigger a caml buffer if needed
+            (tuareg-run-process-if-needed)
+            (pop-to-buffer tuareg-interactive-buffer-name)))
 
-  (defun switch-to-caml-buffer ()
-    "Switch to the caml buffer if needed."
-    (interactive)
-    ;; will trigger a caml buffer if needed
-    (tuareg-run-process-if-needed)
-    (pop-to-buffer tuareg-interactive-buffer-name))
+  :config (custom-set-variables '(tuareg-comment-end-extra-indent 1) ;; multi line comments
+                                '(tuareg-interactive-scroll-to-bottom-on-output t)) ;; make the interactive buffer scroll if needed
 
-  ;; display-buffer--same-window-action (to permit the swich keeping the same window)
-  ;; display-buffer--other-frame-action (in another frame)
-
-  ;; multi line comments
-  (setq tuareg-comment-end-extra-indent 1)
-
-  ;; make the interactive buffer scroll if needed
-  (setq tuareg-interactive-scroll-to-bottom-on-output t)
-
-  (define-key tuareg-mode-map (kbd "C-c C-z") 'switch-to-caml-buffer))
-
+  :bind ("C-c C-z" . switch-to-caml-buffer))
 
 (provide 'caml-pack)
 ;;; caml-pack ends here
