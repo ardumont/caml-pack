65b6013e18636f796350f59c251ac495f7d6a91b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 21:30:55 +0200

    Improve readability

--- caml-pack.el
+++ caml-pack.el
@@ -7,17 +7,17 @@
 (use-package smartscan)
 
 (use-package tuareg
-  :init (progn
-          (add-hook 'tuareg-mode-hook (lambda () (smartscan-mode)))
-          (defun switch-to-caml-buffer ()
-            "Switch to the caml buffer if needed."
-            (interactive)
-            ;; will trigger a caml buffer if needed
-            (tuareg-run-process-if-needed)
-            (pop-to-buffer tuareg-interactive-buffer-name)))
+  :init (defun switch-to-caml-buffer ()
+          "Switch to the caml buffer if needed."
+          (interactive)
+          ;; will trigger a caml buffer if needed
+          (tuareg-run-process-if-needed)
+          (pop-to-buffer tuareg-interactive-buffer-name))
 
-  :config (custom-set-variables '(tuareg-comment-end-extra-indent 1) ;; multi line comments
-                                '(tuareg-interactive-scroll-to-bottom-on-output t)) ;; make the interactive buffer scroll if needed
+  :config (progn
+            (add-hook 'tuareg-mode-hook 'smartscan-mode)
+            (custom-set-variables '(tuareg-comment-end-extra-indent 1)             ;; multi line comments
+                                  '(tuareg-interactive-scroll-to-bottom-on-output t))) ;; make the interactive buffer scroll if needed
 
   :bind ("C-c C-z" . switch-to-caml-buffer))
 
