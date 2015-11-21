dffe2e5d9f9a5fe258e0a40def71c5c62c8f9df0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-11-21 10:55:37 +0100

    Bind locally and not globally

--- caml-pack.el
+++ caml-pack.el
@@ -14,16 +14,16 @@
           (tuareg-run-process-if-needed)
           (pop-to-buffer tuareg-interactive-buffer-name))
 
+  (bind-key "C-c C-z" 'switch-to-caml-buffer tuareg-mode-map)
+  (bind-key "C-c C-l" 'tuareg-eval-buffer    tuareg-mode-map)
+
   :config (progn
             (add-hook 'tuareg-mode-hook 'smartscan-mode)
             (custom-set-variables
              ;; multi line comments
              '(tuareg-comment-end-extra-indent 1)
              ;; make the interactive buffer scroll if needed
-             '(tuareg-interactive-scroll-to-bottom-on-output t)))
-
-  :bind (("C-c C-z" . switch-to-caml-buffer)
-         ("C-c C-l" . tuareg-eval-buffer)))
+             '(tuareg-interactive-scroll-to-bottom-on-output t))))
 
 (provide 'caml-pack)
 ;;; caml-pack ends here
