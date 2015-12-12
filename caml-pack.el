9653b66d36a4eda9b43dd8dae10ec3ee91f4115a
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2015-12-12 10:51:02 +0100

    Use company instead of auto-complete

--- caml-pack.el
+++ caml-pack.el
@@ -52,13 +52,13 @@
 ;; Add opam emacs directory to the load-path
 (defconst opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+;; enable company for merlin
+(require 'company)
 ;; Load merlin-mode
-(require merlin)
+(require 'merlin)
 ;; Start merlin on ocaml files
 (add-hook 'tuareg-mode-hook 'merlin-mode t)
 (add-hook 'caml-mode-hook 'merlin-mode t)
-;; Enable auto-complete
-(setq merlin-use-auto-complete-mode 'easy)
 ;; Use opam switch to lookup ocamlmerlin binary
 (setq merlin-command 'opam)
 
